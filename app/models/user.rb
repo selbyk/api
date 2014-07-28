57ac386ee2abea2c8cd5ba3db83f68917ee4b9df
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-07-27 20:47:34 -0700

    add user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,28 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 4 }
 
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  
+  has_many :reverse_relationships, foreign_key: 'followed_id',
+                                   class_name:  'Relationship',
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   before_save do
     self.email   = email.downcase
     self.api_key = SecureRandom.hex(16)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 end
